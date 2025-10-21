1bcac494710225e196a582947e0935c91b396447
Sim-Go <kritik.tv2022@gmail.com>
2025-10-21 10:22:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ st.set_page_config(page_title="Analyse énergétique - Oise", layout="wide")
 # ==============================================================
 
 st.markdown("""## 1) Problématique réelle (Lien avec la spécialité **GRT**)""")
-st.markdown("""### Comment relier les consommations énergétiques de certains secteurs des communes de l’Oise pour pouvoir estimer celles d’autres secteurs dans ces mêmes territoires ?""")
+st.markdown("""### Comment relier les consommations énergétiques de certains secteurs des communes de l’Oise 
+pour pouvoir estimer celles d’autres secteurs dans ces mêmes territoires ?""")
 
 # ==============================================================
 # 2) FORMULATION DE LA QUESTION PRATIQUE — APPROCHE DATA SCIENCE
